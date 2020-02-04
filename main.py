b2f79ca1165b38f415b73b5d5aa76c59fbd5a4c4
Vikram Deshmukh <vikram.deshmukh@volkswagen.co.in>
2020-02-04 06:53:34 +0100

    python code for variables

---
+++ main.py
@@ -0,0 +1,26 @@
+"""
+Below are the details about song Gone gone gone by philips
+"""
+
+# I am declaring variables
+Original_Release_Date = "November 19, 2012"
+Release_Date = "November 19, 2013"
+Label = "19 Recordings/Interscope"
+Copyright = "℗© 2013 19 Recordings, Inc."
+Record_Company_Required_Metadata = "Music file metadata contains unique purchase identifier. Learn more."
+DurationInMinutes,Rating = 3.29, 5
+Genres = "Pop"
+Asin = "B00GKA34ZW"
+
+
+# I am calling out those variables
+print("original Release date:", Original_Release_Date)
+print("Release Date:", Release_Date)
+print("Label:", Label)
+print("Copyright:", Copyright)
+print("Record Company Required Metadata:", Record_Company_Required_Metadata)
+print("DurationInMinutes:", DurationInMinutes)
+print("Genres:", Genres)
+print("Asin:", Asin)
+print("Rating:", Rating)
+
