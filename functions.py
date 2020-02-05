a6426b68d17f3837c0568cbe195893eb49f49dff
Vikram Deshmukh <vikram.deshmukh@volkswagen.co.in>
2020-02-05 10:17:45 +0100

    functions

---
+++ functions.py
@@ -0,0 +1,28 @@
+# functions
+
+'''
+Calling the functions
+'''
+
+def artist():
+    print("Philips")
+artist()
+def year():
+    print("2020")
+year()
+def genre():
+    print("music")
+genre()
+
+'''
+calling boolean value
+'''
+
+def intrest(value):
+    print("intrest:", value)
+    print(bool(value))
+intrest("100%")
+
+def intrest():
+    print(bool())
+intrest()
